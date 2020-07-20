2eb8b3bc535a319eb6fc70f039fc3b4f102a5599
Sumit Bhardwaj <sumitbhardwaj@192-168-1-103.tpgi.com.au>
2020-07-20 17:57:23 +1000

    first commit for part 2

---
+++ Statistical Thinking in Python-Part2/Parameter estimation by optimization.py
@@ -0,0 +1,75 @@
+#*********Optimal Parameters******************************************#
+
+# How often we get no hitters #
+
+# Seed random number generator
+np.random.seed(42)
+
+# Compute mean no-hitter time: tau
+tau = np.mean(nohitter_times)
+
+# Draw out of an exponential distribution with parameter tau: inter_nohitter_time
+inter_nohitter_time = np.random.exponential(tau, 100000)
+
+# Plot the PDF and label axes
+_ = plt.hist(inter_nohitter_time,
+             bins= 50, normed= True, histtype='step')
+_ = plt.xlabel('Games between no-hitters')
+_ = plt.ylabel('PDF')
+
+# Show the plot
+plt.show()
+
+#*********************************************************************#
+
+# Do the data follow your story #
+
+# Create an ECDF from real data: x, y
+x, y = ecdf(nohitter_times)
+
+# Create a CDF from theoretical samples: x_theor, y_theor
+x_theor, y_theor = ecdf(inter_nohitter_time)
+
+# Overlay the plots
+plt.plot(x_theor, y_theor)
+plt.plot(x, y, marker= '.', linestyle= 'none')
+
+# Margins and axis labels
+plt.margins(0.02)
+plt.xlabel('Games between no-hitters')
+plt.ylabel('CDF')
+
+# Show the plot
+plt.show()
+
+#*********************************************************************#
+
+# How is this parameter optimal? #
+
+# Plot the theoretical CDFs
+plt.plot(x_theor, y_theor)
+plt.plot(x, y, marker='.', linestyle='none')
+plt.margins(0.02)
+plt.xlabel('Games between no-hitters')
+plt.ylabel('CDF')
+
+# Take samples with half tau: samples_half
+samples_half = np.random.exponential(tau/2,10000)
+
+# Take samples with double tau: samples_double
+samples_double = np.random.exponential(2*tau, 10000)
+
+# Generate CDFs from these samples
+x_half, y_half = ecdf(samples_half)
+x_double, y_double = ecdf(samples_double)
+
+# Plot these CDFs as lines
+_ = plt.plot(x_half, y_half)
+_ = plt.plot(x_double, y_double)
+
+# Show the plot
+plt.show()
+
+#*********************************************************************#
+
+#*******Linear Regression by leat Squares*****************************#
