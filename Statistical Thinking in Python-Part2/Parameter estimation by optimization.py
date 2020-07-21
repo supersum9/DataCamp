32f98f394cdab62ad689913c0d8ccf2383728df8
Sumit Bhardwaj <sumitbhardwaj@192-168-1-103.tpgi.com.au>
2020-07-21 11:43:13 +1000

    chapter 1

--- Statistical Thinking in Python-Part2/Parameter estimation by optimization.py
+++ Statistical Thinking in Python-Part2/Parameter estimation by optimization.py
@@ -142,3 +142,37 @@ plt.show()
 
 #***************The importance of EDA: Anscombe's quartet*************#
 
+#Linear regression on appropriate Anscombe data#
+
+# Perform linear regression: a, b
+a, b = np.polyfit(x, y, 1)
+
+# Print the slope and intercept
+print(a, b)
+
+# Generate theoretical x and y data: x_theor, y_theor
+x_theor = np.array([3, 15])
+y_theor = a * x_theor + b
+
+# Plot the Anscombe data and theoretical line
+_ = plt.plot(x, y, marker='.', linestyle='none')
+_ = plt.plot(x_theor, y_theor, marker='.', linestyle='none')
+
+# Label the axes
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Show the plot
+plt.show()
+
+#Linear regression on all Anscombe data#
+
+# Iterate through x,y pairs
+for x, y in zip(anscombe_x, anscombe_y):
+    # Compute the slope and intercept: a, b
+    a, b = np.polyfit(x, y, 1)
+
+    # Print the result
+    print('slope:', a, 'intercept:', b)
+
+#*********************************************************************#
