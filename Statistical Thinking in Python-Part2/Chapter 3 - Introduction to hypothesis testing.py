bcc2073f1b10e12fc335c507b9439f810eec599c
Sumit Bhardwaj <sumitbhardwaj@192-168-1-103.tpgi.com.au>
2020-07-22 11:52:32 +1000

    chapter 3

---
+++ Statistical Thinking in Python-Part2/Chapter 3 - Introduction to hypothesis testing.py
@@ -0,0 +1,53 @@
+#**************Formulating and simulating a hypothesis****************#
+
+#Generating a permutation sample#
+def permutation_sample(data1, data2):
+    """Generate a permutation sample from two data sets."""
+
+    # Concatenate the data sets: data
+    data = np.concatenate((data1, data2))
+
+    # Permute the concatenated array: permuted_data
+    permuted_data = np.random.permutation(data)
+
+    # Split the permuted array into two: perm_sample_1, perm_sample_2
+    perm_sample_1 = permuted_data[:len(data1)]
+    perm_sample_2 = permuted_data[len(data1):]
+
+    return perm_sample_1, perm_sample_2
+
+#*********************************************************************#
+
+#Visualizing permutation sampling#
+
+for i in range(50):
+    # Generate permutation samples
+    perm_sample_1, perm_sample_2 = permutation_sample\
+    (rain_june, rain_november)
+
+
+    # Compute ECDFs
+    x_1, y_1 = ecdf(perm_sample_1)
+    x_2, y_2 = ecdf(perm_sample_2)
+
+    # Plot ECDFs of permutation sample
+    _ = plt.plot(x_1, y_1, marker='.', linestyle='none',
+                 color='red', alpha=0.02)
+    _ = plt.plot(x_2, y_2, marker='.', linestyle='none',
+                 color='blue', alpha=0.02)
+
+# Create and plot ECDFs from original data
+x_1, y_1 = ecdf(rain_june)
+x_2, y_2 = ecdf(rain_november)
+_ = plt.plot(x_1, y_1, marker='.', linestyle='none', color='red')
+_ = plt.plot(x_2, y_2, marker='.', linestyle='none', color='blue')
+
+# Label axes, set margin, and show plot
+plt.margins(0.02)
+_ = plt.xlabel('monthly rainfall (mm)')
+_ = plt.ylabel('ECDF')
+plt.show()
+
+#*********************************************************************#
+
+#*******************Test statistics and p-values**********************#
