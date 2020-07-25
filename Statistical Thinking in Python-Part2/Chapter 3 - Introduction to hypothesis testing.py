a53783940a60e0baf1ef26e997f1441c46f35759
Sumit Bhardwaj <sumitbhardwaj@Sumits-MacBook-Pro.local>
2020-07-25 12:25:24 +1000

    p-value commit

--- Statistical Thinking in Python-Part2/Chapter 3 - Introduction to hypothesis testing.py
+++ Statistical Thinking in Python-Part2/Chapter 3 - Introduction to hypothesis testing.py
@@ -68,3 +68,42 @@ def draw_perm_reps(data_1, data_2, func, size=1):
         perm_replicates[i] = func(perm_sample_1, perm_sample_2)
 
     return perm_replicates
+
+#*********************************************************************#
+
+#Look before you leap: EDA before hypothesis testing#
+
+# Make bee swarm plot
+_ = sns.swarmplot(x='ID', y='impact_force', data=df)
+
+# Label axes
+_ = plt.xlabel('frog')
+_ = plt.ylabel('impact force (N)')
+
+# Show the plot
+plt.show()
+
+#*********************************************************************#
+
+#Permutation test on frog data#
+
+def diff_of_means(data_1, data_2):
+    """Difference in means of two arrays."""
+
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1) - np.mean(data_2)
+
+    return diff
+
+# Compute difference of mean impact force from experiment: empirical_diff_means
+empirical_diff_means = diff_of_means(force_a, force_b)
+
+# Draw 10,000 permutation replicates: perm_replicates
+perm_replicates = draw_perm_reps(force_a, force_b,
+                                 diff_of_means, size=10000)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= empirical_diff_means) / len(perm_replicates)
+
+# Print the result
+print('p-value =', p)
