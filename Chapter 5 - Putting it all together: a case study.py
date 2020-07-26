f3ee5825fa81ef244cdeaae3d2b6319bc70176ca
Sumit Bhardwaj <sumitbhardwaj@192-168-1-105.tpgi.com.au>
2020-07-26 15:57:13 +1000

    code commit

--- Chapter 5 - Putting it all together: a case study.py
+++ Chapter 5 - Putting it all together: a case study.py
@@ -82,3 +82,221 @@ print('p =', p)
 
 #*****************Variation in beak shapes****************************#
 
+#EDA of beak length and depth#
+
+# Make scatter plot of 1975 data
+_ = plt.plot(bl_1975, bd_1975, marker='.',
+             linestyle='None', color='blue', alpha=0.5)
+
+# Make scatter plot of 2012 data
+_ = plt.plot(bl_2012, bd_2012, marker='.',
+            linestyle='None', color='red', alpha=0.5)
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Show the plot
+plt.show()
+#*********************************************************************#
+
+#Linear regressions#
+
+# Compute the linear regressions
+slope_1975, intercept_1975 = np.polyfit(bl_1975, bd_1975, 1)
+slope_2012, intercept_2012 = np.polyfit(bl_2012, bd_2012, 1)
+
+# Perform pairs bootstrap for the linear regressions
+bs_slope_reps_1975, bs_intercept_reps_1975 = \
+        draw_bs_pairs_linreg(bl_1975, bd_1975, size=1000 )
+bs_slope_reps_2012, bs_intercept_reps_2012 = \
+        draw_bs_pairs_linreg(bl_2012, bd_2012, size=1000 )
+
+# Compute confidence intervals of slopes
+slope_conf_int_1975 = np.percentile(bs_slope_reps_1975,[2.5,97.5])
+slope_conf_int_2012 = np.percentile(bs_slope_reps_2012, [2.5,97.5])
+intercept_conf_int_1975 = np.percentile(bs_intercept_reps_1975,[2.5,97.5])
+
+intercept_conf_int_2012 = np.percentile(bs_intercept_reps_2012,[2.5,97.5])
+
+
+# Print the results
+print('1975: slope =', slope_1975,
+      'conf int =', slope_conf_int_1975)
+print('1975: intercept =', intercept_1975,
+      'conf int =', intercept_conf_int_1975)
+print('2012: slope =', slope_2012,
+      'conf int =', slope_conf_int_2012)
+print('2012: intercept =', intercept_2012,
+      'conf int =', intercept_conf_int_2012)
+
+#*********************************************************************#
+
+#Displaying the linear regression results#
+
+# Make scatter plot of 1975 data
+_ = plt.plot(bl_1975, bd_1975, marker='.',
+             linestyle='none', color='blue', alpha=0.5)
+
+# Make scatter plot of 2012 data
+_ = plt.plot(bl_2012, bd_2012, marker='.',
+             linestyle='none', color='red', alpha=0.5)
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Generate x-values for bootstrap lines: x
+x = np.array([10, 17])
+
+# Plot the bootstrap lines
+for i in range(100):
+    plt.plot(x, bs_slope_reps_1975[i]*x + bs_intercept_reps_1975[i],
+             linewidth=0.5, alpha=0.2, color='blue')
+    plt.plot(x ,bs_slope_reps_2012[i]*x + bs_intercept_reps_2012[i],
+             linewidth=0.5, alpha=0.2, color='red')
+
+# Draw the plot again
+plt.show()
+
+#*********************************************************************#
+
+#Beak length to depth ratio#
+
+# Compute length-to-depth ratios
+ratio_1975 = bl_1975/bd_1975
+ratio_2012 = bl_2012/bd_2012
+
+# Compute means
+mean_ratio_1975 = np.mean(ratio_1975)
+mean_ratio_2012 = np.mean(ratio_2012)
+
+# Generate bootstrap replicates of the means
+bs_replicates_1975 = draw_bs_reps(ratio_1975, np.mean, 10000)
+bs_replicates_2012 = draw_bs_reps(ratio_2012, np.mean, 10000)
+
+# Compute the 99% confidence intervals
+conf_int_1975 = np.percentile(bs_replicates_1975,[0.5,99.5])
+conf_int_2012 = np.percentile(bs_replicates_2012,[0.5,99.5])
+
+# Print the results
+print('1975: mean ratio =', mean_ratio_1975,
+      'conf int =', conf_int_1975)
+print('2012: mean ratio =', mean_ratio_2012,
+      'conf int =', conf_int_2012)
+#*********************************************************************#
+
+#**************************Calculation of heritability****************#
+
+#EDA of heritability#
+
+# Make scatter plots
+_ = plt.plot(bd_parent_fortis, bd_offspring_fortis,
+             marker='.', linestyle='none', color='blue', alpha=0.5)
+_ = plt.plot(bd_parent_scandens, bd_offspring_scandens,
+             marker='.', linestyle='none', color='red', alpha=0.5)
+
+# Label axes
+_ = plt.xlabel('parental beak depth (mm)')
+_ = plt.ylabel('offspring beak depth (mm)')
+
+# Add legend
+_ = plt.legend(('G. fortis', 'G. scandens'), loc='lower right')
+
+# Show plot
+plt.show()
+
+#*********************************************************************#
+
+#Correlation of offspring and parental data#
+
+def draw_bs_pairs(x, y, func, size=1):
+    """Perform pairs bootstrap for a single statistic."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_replicates[i] = func(bs_x, bs_y)
+
+    return bs_replicates
+
+#*********************************************************************#
+
+#Pearson correlation of offspring and parental data#
+
+# Compute the Pearson correlation coefficients
+r_scandens = pearson_r(bd_parent_scandens, bd_offspring_scandens)
+r_fortis = pearson_r(bd_parent_fortis, bd_offspring_fortis)
+
+# Acquire 1000 bootstrap replicates of Pearson r
+bs_replicates_scandens = draw_bs_pairs(bd_parent_scandens, bd_offspring_scandens, pearson_r, 1000)
+
+bs_replicates_fortis = draw_bs_pairs(bd_parent_fortis, bd_offspring_fortis, pearson_r, 1000)
+
+
+# Compute 95% confidence intervals
+conf_int_scandens = np.percentile(bs_replicates_scandens,[2.5, 97.5])
+conf_int_fortis = np.percentile(bs_replicates_fortis, [2.5, 97.5])
+
+# Print results
+print('G. scandens:', r_scandens, conf_int_scandens)
+print('G. fortis:', r_fortis, conf_int_fortis)
+
+#*********************************************************************#
+
+#Measuring heritability#
+
+def heritability(parents, offspring):
+    """Compute the heritability from parent and offspring samples."""
+    covariance_matrix = np.cov(parents, offspring)
+    return covariance_matrix[0,1] / covariance_matrix[0,0]
+
+# Compute the heritability
+heritability_scandens = heritability(bd_parent_scandens, bd_offspring_scandens)
+heritability_fortis = heritability(bd_parent_fortis, bd_offspring_fortis)
+
+# Acquire 1000 bootstrap replicates of heritability
+replicates_scandens = draw_bs_pairs(
+        bd_parent_scandens, bd_offspring_scandens, heritability, size=1000)
+
+replicates_fortis = draw_bs_pairs(
+        bd_parent_fortis, bd_offspring_fortis, heritability, size=1000)
+
+# Compute 95% confidence intervals
+conf_int_scandens = np.percentile(replicates_scandens,[2.5,97.5])
+conf_int_fortis = np.percentile(replicates_fortis, [2.5, 97.5])
+
+# Print results
+print('G. scandens:', heritability_scandens, conf_int_scandens)
+print('G. fortis:', heritability_fortis, conf_int_fortis)
+
+#*********************************************************************#
+
+#Is beak depth heritable at all in G. scandens?#
+
+# Initialize array of replicates: perm_replicates
+perm_replicates = np.empty(10000)
+
+# Draw replicates
+for i in range(10000):
+    # Permute parent beak depths
+    bd_parent_permuted = np.random.permutation(bd_parent_scandens)
+    perm_replicates[i] = heritability(bd_parent_permuted, bd_offspring_scandens)
+
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= heritability_scandens) / len(perm_replicates)
+
+# Print the p-value
+print('p-val =', p)
+
+#*********************************************************************#
