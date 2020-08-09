6182c258bc331d9083e079a83b39b1db42d51ad7
Sumit Bhardwaj <sumitbhardwaj@192-168-1-106.tpgi.com.au>
2020-08-09 12:54:10 +1000

    code commit

--- Unsupervised Learning in Python/Chapter 2 - Visualization with hierarchical clustering and t-SNE.py
+++ Unsupervised Learning in Python/Chapter 2 - Visualization with hierarchical clustering and t-SNE.py
@@ -1,1 +1,40 @@
 #***********************Visualizing hierarchies***********************#
+#Hierarchical clustering of the grain data#
+
+# Perform the necessary imports
+from scipy.cluster.hierarchy import linkage, dendrogram
+import matplotlib.pyplot as plt
+
+# Calculate the linkage: mergings
+mergings = linkage(samples, method= 'complete')
+
+# Plot the dendrogram, using varieties as labels
+dendrogram(mergings,
+           labels=varieties,
+           leaf_rotation=90,
+           leaf_font_size=6,
+)
+plt.show()
+
+#*********************************************************************#
+
+#Hierarchies of stocks#
+
+# Import normalize
+from sklearn.preprocessing import normalize
+
+# Normalize the movements: normalized_movements
+normalized_movements = normalize(movements)
+
+# Calculate the linkage: mergings
+mergings = linkage(normalized_movements, method='complete')
+
+# Plot the dendrogram
+dendrogram(mergings,\
+           labels= companies,\
+           leaf_rotation=90,\
+           leaf_font_size=6)
+plt.show()
+
+#*********************************************************************#
+
