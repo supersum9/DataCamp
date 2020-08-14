96a068dbd9d38bb5e877b9fc9ee4b70027b680c1
Sumit Bhardwaj <sumitbhardwaj@192-168-1-105.tpgi.com.au>
2020-08-14 13:24:15 +1000

    code commit

--- Machine Learning with Tree-Based Models in Python/Chapter 3 - Bagging and Random Forests.py
+++ Machine Learning with Tree-Based Models in Python/Chapter 3 - Bagging and Random Forests.py
@@ -1,2 +1,74 @@
 #********************Bagging******************************************#
 
+#Define the bagging classifier#
+
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(random_state=1)
+
+# Instantiate bc
+bc = BaggingClassifier(base_estimator=dt, n_estimators=50, random_state=1)
+
+#*********************************************************************#
+
+#Evaluate Bagging performance#
+
+# Fit bc to the training set
+bc.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = bc.predict(X_test)
+
+# Evaluate acc_test
+acc_test = accuracy_score(y_test, y_pred)
+print('Test set accuracy of bc: {:.2f}'.format(acc_test))
+
+#*********************************************************************#
+
+#**********************Out of Bag Evaluation**************************#
+
+#Prepare the ground#
+
+# Import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier
+
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf=8, random_state=1)
+
+# Instantiate bc
+bc = BaggingClassifier(base_estimator=dt,
+            n_estimators=50,
+            oob_score=True,
+            random_state=1)
+
+#*********************************************************************#
+
+#OOB Score vs Test Set Score#
+
+# Fit bc to the training set
+bc.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = bc.predict(X_test)
+
+# Evaluate test set accuracy
+acc_test = accuracy_score(y_test, y_pred)
+
+# Evaluate OOB accuracy
+acc_oob = bc.oob_score_
+
+# Print acc_test and acc_oob
+print('Test set accuracy: {:.3f}, OOB accuracy: {:.3f}'.format(acc_test, acc_oob))
+
+#*********************************************************************#
+
+#*************************Random Forests (RF)*************************#
+
