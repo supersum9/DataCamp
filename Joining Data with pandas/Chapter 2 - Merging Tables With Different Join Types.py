525058d2bb2a86a0eddacc6ff79bedcf21812327
Sumit Bhardwaj <sumitbhardwaj@192-168-1-105.tpgi.com.au>
2020-08-22 16:08:51 +1000

    chap 3

--- Joining Data with pandas/Chapter 2 - Merging Tables With Different Join Types.py
+++ Joining Data with pandas/Chapter 2 - Merging Tables With Different Join Types.py
@@ -84,3 +84,51 @@ print(begins_returns[m].head())
 
 #*********************************************************************#
 
+#***********************Merging a table to itself*********************#
+
+#Self join#
+
+# Merge the crews table to itself
+crews_self_merged = crews.merge(crews, on='id', how='inner',
+                                suffixes=('_dir','_crew'))
+
+# Create a boolean index to select the appropriate rows
+boolean_filter = ((crews_self_merged['job_dir'] == 'Director') &
+                  (crews_self_merged['job_crew'] != 'Director'))
+direct_crews = crews_self_merged[boolean_filter]
+
+# Print the first few rows of direct_crews
+print(direct_crews.head())
+
+#*********************************************************************#
+
+#**************************Merging on indexes*************************#
+
+#Index merge for movie ratings#
+
+# Merge to the movies table the ratings table on the index
+movies_ratings = movies.merge(ratings, on='id')
+
+# Print the first few rows of movies_ratings
+print(movies_ratings.head())
+
+#Do sequels earn more?#
+
+# Merge sequels and financials on index id
+sequels_fin = sequels.merge(financials, on='id', how='left')
+
+# Self merge with suffixes as inner join with left on sequel and right on id
+orig_seq = sequels_fin.merge(sequels_fin, how='inner', left_on='sequel',
+                             right_on='id', right_index=True,
+                             suffixes=('_org','_seq'))
+
+# Add calculation to subtract revenue_org from revenue_seq
+orig_seq['diff'] = orig_seq['revenue_seq'] - orig_seq['revenue_org']
+
+# Select the title_org, title_seq, and diff
+titles_diff = orig_seq[['title_org','title_seq','diff']]
+
+# Print the first rows of the sorted titles_diff
+print(titles_diff.sort_values('diff', ascending=False).head())
+
+#*********************************************************************#
