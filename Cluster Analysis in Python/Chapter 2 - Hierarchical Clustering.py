1ccbb004876652d9454b3cbd914f0790b8c74156
Sumit Bhardwaj <sumitbhardwaj@192-168-1-105.tpgi.com.au>
2020-08-22 10:13:23 +1000

    chap2

--- Cluster Analysis in Python/Chapter 2 - Hierarchical Clustering.py
+++ Cluster Analysis in Python/Chapter 2 - Hierarchical Clustering.py
@@ -1,1 +1,119 @@
 #************Basics of hierarchical clustering************************#
+
+#Hierarchical clustering: ward method#
+
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster, linkage
+
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method = 'ward', metric = 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled',
+                hue='cluster_labels', data = comic_con)
+plt.show()
+
+#*********************************************************************#
+
+#Hierarchical clustering: single method#
+
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster, linkage
+
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method = 'single', metric = 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled',
+                hue='cluster_labels', data = comic_con)
+plt.show()
+
+#*********************************************************************#
+
+#Hierarchical clustering: complete method#
+
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster, linkage
+
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled','y_scaled']], method= 'complete', metric= 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2, criterion='maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled',
+                hue='cluster_labels', data = comic_con)
+plt.show()
+
+#*********************************************************************#
+
+#*******************Visualize clusters********************************#
+
+#Visualize clusters with matplotlib#
+
+# Import the pyplot class
+import matplotlib.pyplot as plt
+
+# Define a colors dictionary for clusters
+colors = {1:'red', 2:'blue'}
+
+# Plot a scatter plot
+comic_con.plot.scatter(x='x_scaled',
+                	   y='y_scaled',
+                	   c=comic_con['cluster_labels'].apply(lambda x: colors[x]))
+plt.show()
+
+#*********************************************************************#
+
+#Visualize clusters with seaborn#
+
+# Import the seaborn module
+import seaborn as sns
+
+# Plot a scatter plot using seaborn
+sns.scatterplot(x='x_scaled',
+                y='y_scaled',
+                hue='cluster_labels',
+                data = comic_con)
+plt.show()
+
+#*********************************************************************#
+
+#*************************How many clusters?**************************#
+
+#Create a dendrogram#
+
+# Import the dendrogram function
+from scipy.cluster.hierarchy import dendrogram
+
+# Create a dendrogram
+dn = dendrogram(linkage(comic_con[['x_scaled','y_scaled']], method= 'ward', metric='euclidean'))
+
+# Display the dendogram
+plt.show()
+
+#*********************************************************************#
+
+#FIFA 18: exploring defenders#
+
+# Fit the data into a hierarchical clustering algorithm
+distance_matrix = linkage(fifa[['scaled_sliding_tackle', 'scaled_aggression']], 'ward')
+
+# Assign cluster labels to each row of data
+fifa['cluster_labels'] = fcluster(distance_matrix, 3, criterion='maxclust')
+
+# Display cluster centers of each cluster
+print(fifa[['scaled_sliding_tackle', 'scaled_aggression', 'cluster_labels']].groupby('cluster_labels').mean())
+
+# Create a scatter plot through seaborn
+sns.scatterplot(x='scaled_sliding_tackle', y='scaled_aggression', hue='cluster_labels', data=fifa)
+plt.show()
+
+#*********************************************************************#
