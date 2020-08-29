a81cecc5bc7c943002f96d85252797ada68e88e2
Sumit Bhardwaj <sumitbhardwaj@192-168-1-107.tpgi.com.au>
2020-08-29 14:00:06 +1000

    code

--- Linear Classifiers in Python/Chapter 3 - Logistic regression.py
+++ Linear Classifiers in Python/Chapter 3 - Logistic regression.py
@@ -1,1 +1,65 @@
 #***************Logistic regression and regularization****************#
+
+#Regularized logistic regression#
+
+# Train and validaton errors initialized as empty list
+train_errs = list()
+valid_errs = list()
+
+# Loop over values of C_value
+for C_value in [0.001, 0.01, 0.1, 1, 10, 100, 1000]:
+    # Create LogisticRegression object and fit
+    lr = LogisticRegression(C=C_value)
+    lr.fit(X_train, y_train)
+
+    # Evaluate error rates and append to lists
+    train_errs.append( 1.0 - lr.score(X_train,y_train))
+    valid_errs.append( 1.0 - lr.score(X_valid,y_valid))
+
+# Plot results
+plt.semilogx(C_values, train_errs, C_values, valid_errs)
+plt.legend(("train", "validation"))
+plt.show()
+
+#*********************************************************************#
+
+#Logistic regression and feature selection#
+
+# Specify L1 regularization
+lr = LogisticRegression(penalty='l1')
+
+# Instantiate the GridSearchCV object and run the search
+searcher = GridSearchCV(lr, {'C':[0.001, 0.01, 0.1, 1, 10]})
+searcher.fit(X_train, y_train)
+
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
+
+# Find the number of nonzero coefficients (selected features)
+best_lr = searcher.best_estimator_
+coefs = best_lr.coef_
+print("Total number of features:", coefs.size)
+print("Number of selected features:", np.count_nonzero(coefs))
+
+#*********************************************************************#
+
+#Identifying the most positive and negative words#
+
+# Get the indices of the sorted cofficients
+inds_ascending = np.argsort(lr.coef_.flatten())
+inds_descending = inds_ascending[::-1]
+
+# Print the most positive words
+print("Most positive words: ", end="")
+for i in range(5):
+    print(vocab[inds_descending[i]], end=", ")
+print("\n")
+
+# Print most negative words
+print("Most negative words: ", end="")
+for i in range(5):
+    print(vocab[inds_ascending[i]], end=", ")
+print("\n")
+
+#*********************************************************************#
+
