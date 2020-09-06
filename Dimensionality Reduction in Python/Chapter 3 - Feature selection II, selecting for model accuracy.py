edb382f576f66564df9003115f12c3cb9088fef4
Sumit Bhardwaj <sumitbhardwaj@192-168-1-107.tpgi.com.au>
2020-09-06 20:33:46 +1000

    code

---
+++ Dimensionality Reduction in Python/Chapter 3 - Feature selection II, selecting for model accuracy.py
@@ -0,0 +1,243 @@
+#*************Selecting features for model performance*************************#
+
+#Building a diabetes classifier#
+
+# Fit the scaler on the training features and transform these in one go
+X_train_std = scaler.fit_transform(X_train)
+
+# Fit the logistic regression model on the scaled training data
+lr.fit(X_train_std, y_train)
+
+# Scale the test features
+X_test_std = scaler.transform(X_test)
+
+# Predict diabetes presence on the scaled test set
+y_pred = lr.predict(X_test_std)
+
+# Prints accuracy metrics and feature coefficients
+print("{0:.1%} accuracy on test set.".format(accuracy_score(y_test, y_pred)))
+print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
+
+#******************************************************************************#
+
+#Manual Recursive Feature Elimination#
+
+# Only keep the feature with the highest coefficient
+X = diabetes_df[['glucose', 'triceps', 'bmi', 'family', 'age']]
+
+# Performs a 25-75% train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Scales features and fits the logistic regression model to the data
+lr.fit(scaler.fit_transform(X_train), y_train)
+
+# Calculates the accuracy on the test set and prints coefficients
+acc = accuracy_score(y_test, lr.predict(scaler.transform(X_test)))
+print("{0:.1%} accuracy on test set.".format(acc))
+print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
+
+#******************************************************************************#
+
+#Automatic Recursive Feature Elimination#
+
+# Create the RFE with a LogisticRegression estimator and 3 features to select
+rfe = RFE(estimator=LogisticRegression(), n_features_to_select=3, verbose=1)
+
+# Fits the eliminator to the data
+rfe.fit(X_train, y_train)
+
+# Print the features and their ranking (high = dropped early on)
+print(dict(zip(X.columns, rfe.ranking_)))
+
+# Print the features that are not eliminated
+print(X.columns[rfe.support_])
+
+# Calculates the test set accuracy
+acc = accuracy_score(y_test, rfe.predict(X_test))
+print("{0:.1%} accuracy on test set.".format(acc))
+
+#******************************************************************************#
+
+#Building a random forest model#
+
+# Perform a 75% training and 25% test data split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Fit the random forest model to the training data
+rf = RandomForestClassifier(random_state=0)
+rf.fit(X_train, y_train)
+
+# Calculate the accuracy
+acc = accuracy_score(y_test, rf.predict(X_test))
+
+# Print the importances per feature
+print(dict(zip(X.columns, rf.feature_importances_.round(2))))
+
+# Print accuracy
+print("{0:.1%} accuracy on test set.".format(acc))
+
+#******************************************************************************#
+
+#Random forest for feature selection#
+
+# Create a mask for features importances above the threshold
+mask = rf.feature_importances_ > 0.15
+
+# Apply the mask to the feature dataset X
+reduced_X = X.loc[:,mask]
+
+# prints out the selected column names
+print(reduced_X.columns)
+
+#******************************************************************************#
+
+#Recursive Feature Elimination with random forests#
+
+# Set the feature eliminator to remove 2 features on each step
+rfe = RFE(estimator=RandomForestClassifier(), n_features_to_select=2, step=2, verbose=1)
+
+# Fit the model to the training data
+rfe.fit(X_train, y_train)
+
+# Create a mask
+mask = rfe.support_
+
+# Apply the mask to the feature dataset X and print the result
+reduced_X = X.loc[:, mask]
+print(reduced_X.columns)
+
+#******************************************************************************#
+
+#****************************Regularized linearregression**********************#
+
+#Creating a LASSO regressor#
+
+# Set the test size to 30% to get a 70-30% train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+# Fit the scaler on the training features and transform these in one go
+X_train_std = scaler.fit_transform(X_train)
+
+# Create the Lasso model
+la = Lasso()
+
+# Fit it to the standardized training data
+la.fit(X_train_std, y_train)
+
+#******************************************************************************#
+
+#Lasso model results#
+
+# Transform the test set with the pre-fitted scaler
+X_test_std = scaler.transform(X_test)
+
+# Calculate the coefficient of determination (R squared) on X_test_std
+r_squared = la.score(X_test_std, y_test)
+print("The model can predict {0:.1%} of the variance in the test set.".format(r_squared))
+
+# Create a list that has True values when coefficients equal 0
+zero_coef = la.coef_ == 0
+
+# Calculate how many features have a zero coefficient
+n_ignored = sum(zero_coef)
+print("The model has ignored {} out of {} features.".\
+    format(n_ignored, len(la.coef_)))
+
+#******************************************************************************#
+
+#Adjusting the regularization strength#
+
+# Find the highest alpha value with R-squared above 98%
+la = Lasso(alpha=0.1, random_state=0)
+
+# Fits the model and calculates performance stats
+la.fit(X_train_std, y_train)
+r_squared = la.score(X_test_std, y_test)
+n_ignored_features = sum(la.coef_ == 0)
+
+# Print peformance stats
+print("The model can predict {0:.1%} of the variance in the test set.".\
+            format(r_squared))
+print("{} out of {} features were ignored.".\
+            format(n_ignored_features, len(la.coef_)))
+
+#******************************************************************************#
+
+#**********************************Combining feature selectors*****************#
+
+#Creating a LassoCV regressor#
+
+from sklearn.linear_model import LassoCV
+
+# Create and fit the LassoCV model on the training set
+lcv = LassoCV()
+lcv.fit(X_train, y_train)
+print('Optimal alpha = {0:.3f}'.format(lcv.alpha_))
+
+# Calculate R squared on the test set
+r_squared = lcv.score(X_test, y_test)
+print('The model explains {0:.1%} of the test set variance'.format(r_squared))
+
+# Create a mask for coefficients not equal to zero
+lcv_mask = lcv.coef_ !=0
+print('{} features out of {} selected'.format(sum(lcv_mask), len(lcv_mask)))
+
+#******************************************************************************#
+
+#Ensemble models for extra votes#
+
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Select 10 features with RFE on a GradientBoostingRegressor, drop 3 features on each step
+rfe_gb = RFE(estimator=GradientBoostingRegressor(),
+             n_features_to_select=10, step=3, verbose=1)
+rfe_gb.fit(X_train, y_train)
+
+# Calculate the R squared on the test set
+r_squared = rfe_gb.score(X_test, y_test)
+print('The model can explain {0:.1%} of the variance in the test set'.\
+    format(r_squared))
+
+# Assign the support array to gb_mask
+gb_mask = rfe_gb.support_
+
+
+#Random Forest Regressor#
+
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import RandomForestRegressor
+
+# Select 10 features with RFE on a RandomForestRegressor, drop 3 features on each step
+rfe_rf = RFE(estimator=RandomForestRegressor(),
+             n_features_to_select=10, step=3, verbose=1)
+rfe_rf.fit(X_train, y_train)
+
+# Calculate the R squared on the test set
+r_squared = rfe_rf.score(X_test, y_test)
+print('The model can explain {0:.1%} of the variance in the test set'.\
+    format(r_squared))
+
+# Assign the support array to gb_mask
+rf_mask = rfe_rf.support_
+
+#******************************************************************************#
+
+#Combining 3 feature selectors#
+
+# Sum the votes of the three models
+votes = np.sum([lcv_mask, rf_mask, gb_mask], axis=0)
+
+# Create a mask for features selected by all 3 models
+meta_mask = votes >= 3
+
+# Apply the dimensionality reduction on X
+X_reduced = X.loc[:, meta_mask]
+
+# Plug the reduced dataset into a linear regression pipeline
+X_train, X_test, y_train, y_test = train_test_split\
+    (X_reduced, y, test_size=0.3, random_state=0)
+lm.fit(scaler.fit_transform(X_train), y_train)
+r_squared = lm.score(scaler.transform(X_test), y_test)
+print('The model can explain {0:.1%} of the variance in the test set using {1:}\
+     features.'.format(r_squared, len(lm.coef_)))
