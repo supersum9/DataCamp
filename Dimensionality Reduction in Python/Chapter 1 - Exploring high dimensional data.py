6a8eeddf865ada9fce3b3f976fa9393c112a9b42
Sumit Bhardwaj <sumitbhardwaj@192-168-1-107.tpgi.com.au>
2020-09-06 14:50:06 +1000

    code

---
+++ Dimensionality Reduction in Python/Chapter 1 - Exploring high dimensional data.py
@@ -0,0 +1,98 @@
+#**************************Introduction****************************************#
+
+#Removing features without variance#
+
+# Leave this list as is
+number_cols = ['HP', 'Attack', 'Defense']
+
+# Remove the feature without variance from this list
+non_number_cols = ['Name', 'Type']
+
+# Create a new dataframe by subselecting the chosen features
+df_selected = pokemon_df[number_cols + non_number_cols]
+
+# Prints the first 5 lines of the new dataframe
+print(df_selected.head())
+
+#******************************************************************************#
+
+#*******************Feature selection vs feature extraction********************#
+
+#Visually detecting redundant features#
+
+# Create a pairplot and color the points using the 'Gender' feature
+sns.pairplot(ansur_df_1, hue='Gender', diag_kind='hist')
+
+# Show the plot
+plt.show()
+
+# Remove one of the redundant features
+reduced_df = ansur_df_1.drop('stature_m', axis=1)
+
+# Create a pairplot and color the points using the 'Gender' feature
+sns.pairplot(reduced_df, hue='Gender')
+
+# Show the plot
+plt.show()
+
+# Remove the redundant feature
+reduced_df = ansur_df_2.____(____, ____)
+
+# Create a pairplot and color the points using the 'Gender' feature
+sns.pairplot(reduced_df, hue='Gender', diag_kind='hist')
+
+# Show the plot
+plt.show()
+
+# Remove the redundant feature
+reduced_df = ansur_df_2.drop('n_legs', axis=1)
+
+# Create a pairplot and color the points using the 'Gender' feature
+sns.pairplot(reduced_df, hue='Gender', diag_kind='hist')
+
+# Show the plot
+plt.show()
+
+#******************************************************************************#
+
+#*****************t-SNE visualizationof high-dimensionaldata*******************#
+
+#Fitting t-SNE to the ANSUR data#
+
+# Non-numerical columns in the dataset
+non_numeric = ['Branch', 'Gender', 'Component']
+
+# Drop the non-numerical columns from df
+df_numeric = df.drop(non_numeric, axis=1)
+
+# Create a t-SNE model with learning rate 50
+m = TSNE(learning_rate=50)
+
+# Fit and transform the t-SNE model on the numeric dataset
+tsne_features = m.fit_transform(df_numeric)
+print(tsne_features.shape)
+
+#******************************************************************************#
+
+#t-SNE visualisation of dimensionality#
+
+# Color the points according to Army Component
+sns.scatterplot(x="x", y="y", hue='Component', data=df)
+
+# Show the plot
+plt.show()
+
+# Color the points by Army Branch
+sns.scatterplot(x="x", y="y", hue='Branch', data=df)
+
+# Show the plot
+plt.show()
+
+# Color the points by Gender
+sns.scatterplot(x="x", y="y", hue='Gender', data=df)
+
+# Show the plot
+plt.show()
+
+#******************************************************************************#
+
