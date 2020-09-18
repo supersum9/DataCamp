5ed4d7406a4a1a8e5aa62403edbedacdf4ea0b6c
Sumit Bhardwaj <sumitbhardwaj@192-168-1-107.tpgi.com.au>
2020-09-18 14:24:21 +1000

    code

---
+++ Introduction to Airflow in Python/Chapter 1 - Intro to Airflow.py
@@ -0,0 +1,40 @@
+###########################Introduction to Airflow#############################
+
+###############################Airflow DAGs####################################
+
+#Defining a simple DAG#
+
+# Import the DAG object
+from airflow.models import DAG
+
+#########
+
+# Import the DAG object
+from airflow.models import DAG
+
+# Define the default_args dictionary
+default_args = {
+  'owner': 'dsmith',
+  'start_date': datetime(2020, 1, 14),
+  'retries': 2
+}
+
+##########
+
+# Import the DAG object
+from airflow.models import DAG
+
+# Define the default_args dictionary
+default_args = {
+  'owner': 'dsmith',
+  'start_date': datetime(2020, 1, 14),
+  'retries': 2
+}
+
+# Instantiate the DAG object
+etl_dag = DAG('example_etl', default_args=default_args)
+
+###############################################################################
+
+#Troubleshooting DAG creation#
+
