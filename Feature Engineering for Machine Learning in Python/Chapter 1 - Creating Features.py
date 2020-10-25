1ae204899ec41a290f7e63205d54755ad8e2c8b3
supersum9 <sumitbhardwaj@192-168-1-101.tpgi.com.au>
2020-10-25 17:26:19 +1100

    code

--- Feature Engineering for Machine Learning in Python/Chapter 1 - Creating Features.py
+++ Feature Engineering for Machine Learning in Python/Chapter 1 - Creating Features.py
@@ -1,2 +1,30 @@
 #******************************Why generate features?*************************#
 
+#Getting to know your data#
+
+# Import pandas
+import pandas as pd
+
+# Import so_survey_csv into so_survey_df
+so_survey_df = pd.read_csv(so_survey_csv)
+
+# Print the first five rows of the DataFrame
+print(so_survey_df.head())
+
+# Print the data type of each column
+print(so_survey_df.dtypes)
+
+#*****************************************************************************#
+
+#Selecting specific data types#
+
+# Create subset of only the numeric columns
+so_numeric_df = so_survey_df.select_dtypes(include=['float','int'])
+
+# Print the column names contained in so_survey_df_num
+print(so_numeric_df.columns)
+
+#*****************************************************************************#
+
+#**********************Dealing with categorical features**********************#
+
