bfe49b42204f61a22f8049a729e64178d1640554
supersum9 <sumitbhardwaj@192-168-1-101.tpgi.com.au>
2020-10-25 17:12:44 +1100

    code

--- Machine Learning for Time Series Data in Python/Chapter 3 - Predicting Time Series Data.py
+++ Machine Learning for Time Series Data in Python/Chapter 3 - Predicting Time Series Data.py
@@ -43,7 +43,9 @@ from sklearn.metrics import r2_score
 
 # Split our data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    train_size=.8, shuffle=False, random_state=1)
+                                                    train_size=.8, \
+                                                        shuffle=False, \
+                                                        random_state=1)
 
 # Fit our model and generate predictions
 model = Ridge()
