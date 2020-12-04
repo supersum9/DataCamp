0ffcc82806865a332b8fda63340cbea6fb576a72
supersum9 <sumitbhardwaj@192-168-1-101.tpgi.com.au>
2020-12-04 17:35:38 +1100

    code

--- Model Validation In Python/Chapter 2 - Validation Basics.py
+++ Model Validation In Python/Chapter 2 - Validation Basics.py
@@ -1,2 +1,73 @@
 #*********************Creating train, test, and validation datasets***********#
 
+#Create one holdout set#
+
+# Create dummy variables using pandas
+X = pd.get_dummies(tic_tac_toe.iloc[:,0:9])
+y = tic_tac_toe.iloc[:, 9]
+
+# Create training and testing datasets. Use 10% for the test set
+X_train, X_test, y_train, y_test  = train_test_split(X, y, \
+        test_size=0.10, random_state=1111)
+
+#*****************************************************************************#
+
+#Create two holdout sets#
+
+# Create temporary training and final testing datasets
+X_temp, X_test, y_temp, y_test  =\
+    train_test_split(X, y, test_size=0.20, random_state=1111)
+
+# Create the final training and validation datasets
+X_train, X_val, y_train, y_val =\
+    train_test_split(X_temp, y_temp, test_size=0.25, random_state=1111)
+
+#*****************************************************************************#
+
+#**********************Accuracy metrics:regression models*********************#
+
+#Mean absolute error#
+
+from sklearn.metrics import mean_absolute_error
+
+# Manually calculate the MAE
+n = len(predictions)
+mae_one = sum(abs(y_test - predictions)) / n
+print('With a manual calculation, the error is {}'.format(mae_one))
+
+# Use scikit-learn to calculate the MAE
+mae_two = mean_absolute_error(y_test, predictions)
+print('Using scikit-lean, the error is {}'.format(mae_two))
+
+#*****************************************************************************#
+
+#Mean squared error#
+
+from sklearn.metrics import mean_squared_error
+
+n = len(predictions)
+# Finish the manual calculation of the MSE
+mse_one = sum((y_test - predictions)**2) / n
+print('With a manual calculation, the error is {}'.format(mse_one))
+
+# Use the scikit-learn function to calculate MSE
+mse_two = mean_squared_error(y_test, predictions)
+print('Using scikit-lean, the error is {}'.format(mse_two))
+
+#*****************************************************************************#
+
+#Performance on data subsets#
+
+# Find the East conference teams
+east_teams = labels == "E"
+
+# Create arrays for the true and predicted values
+true_east = y_test[east_teams]
+preds_east = predictions[east_teams]
+
+# Print the accuracy metrics
+print('The MAE for East teams is {}'.format(
+    mae(true_east, preds_east)))
+
+# Print the West accuracy
+print('The MAE for West conference is {}'.format(west_error))
