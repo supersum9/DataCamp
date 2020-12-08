cb2cd964c0cc12b276c6c01db1ca379d8077bfd0
supersum9 <sumitbhardwaj@192-168-1-101.tpgi.com.au>
2020-12-08 17:23:07 +1100

    code

--- Model Validation In Python/Chapter 2 - Validation Basics.py
+++ Model Validation In Python/Chapter 2 - Validation Basics.py
@@ -71,3 +71,91 @@ print('The MAE for East teams is {}'.format(
 
 # Print the West accuracy
 print('The MAE for West conference is {}'.format(west_error))
+
+#*****************************************************************************#
+
+#*****************************Classification metrics**************************#
+
+#Confusion matrices#
+
+    # Calculate and print the accuracy
+    accuracy = (324 + 491) / (953)
+    print("The overall accuracy is {0: 0.2f}".format(accuracy))
+
+    # Calculate and print the precision
+    precision = (491) / (491 + 15)
+    print("The precision is {0: 0.2f}".format(precision))
+
+    # Calculate and print the recall
+    recall = (491) / (491 + 123)
+    print("The recall is {0: 0.2f}".format(recall))
+
+#*****************************************************************************#
+
+#Confusion matrices, again#
+
+from sklearn.metrics import confusion_matrix
+
+# Create predictions
+test_predictions = rfc.predict(X_test)
+
+# Create and print the confusion matrix
+cm = confusion_matrix(y_test, test_predictions)
+print(cm)
+
+# Print the true positives (actual 1s that were predicted 1s)
+print("The number of true positives is: {}".format(cm[1, 1]))
+
+#*****************************************************************************#
+
+#Precision vs. recall#
+
+from sklearn.metrics import precision_score
+
+test_predictions = rfc.predict(X_test)
+
+# Create precision or recall score based on the metric you imported
+score = precision_score(y_test, test_predictions)
+
+# Print the final result
+print("The precision value is {0:.2f}".format(score))
+
+#*****************************************************************************#
+
+#*************************The bias-variance trade off*************************#
+
+#Error due to under/over-fitting#
+
+# Update the rfr model
+rfr = RandomForestRegressor(n_estimators=25,
+                            random_state=1111,
+                            max_features=2)
+rfr.fit(X_train, y_train)
+
+# Print the training and testing accuracies
+print('The training error is {0:.2f}'.format(
+  mae(y_train, rfr.predict(X_train))))
+print('The testing error is {0:.2f}'.format(
+  mae(y_test, rfr.predict(X_test))))
+
+#*****************************************************************************#
+
+#Am I underfitting?#
+
+from sklearn.metrics import accuracy_score
+
+test_scores, train_scores = [], []
+for i in [1, 2, 3, 4, 5, 10, 20, 50]:
+    rfc = RandomForestClassifier(n_estimators=i, random_state=1111)
+    rfc.fit(X_train, y_train)
+    # Create predictions for the X_train and X_test datasets.
+    train_predictions = rfc.predict(X_train)
+    test_predictions = rfc.predict(X_test)
+    # Append the accuracy score for the test and train predictions.
+    train_scores.append(round(accuracy_score(y_train, train_predictions), 2))
+    test_scores.append(round(accuracy_score(y_test, test_predictions), 2))
+# Print the train and test scores.
+print("The training scores were: {}".format(train_scores))
+print("The testing scores were: {}".format(test_scores))
+
+#*****************************************************************************#
