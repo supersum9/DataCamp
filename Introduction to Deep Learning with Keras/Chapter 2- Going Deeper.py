bdb389c0a983a3842d6b3ffd71139958ba623058
supersum9 <sumitbhardwaj@192-168-1-101.tpgi.com.au>
2021-01-03 16:20:49 +1100

    code

--- Introduction to Deep Learning with Keras/Chapter 2- Going Deeper.py
+++ Introduction to Deep Learning with Keras/Chapter 2- Going Deeper.py
@@ -129,3 +129,93 @@ for i,pred in enumerate(preds_chosen):
 
 #***********************Multi-label classification****************************#
 
+#An irrigation machine#
+
+# Instantiate a Sequential model
+model = Sequential()
+
+# Add a hidden layer of 64 neurons and a 20 neuron's input
+model.add(Dense(64, input_shape=(20,), activation='relu'))
+
+# Add an output layer of 3 neurons with sigmoid activation
+model.add(Dense(3, activation='sigmoid'))
+
+# Compile your model with binary crossentropy loss
+model.compile(optimizer = 'adam',
+              loss = 'binary_crossentropy',
+              metrics = ['accuracy'])
+
+model.summary()
+
+#*****************************************************************************#
+
+#Training with multiple labels#
+
+# Train for 100 epochs using a validation split of 0.2
+model.fit(sensors_train, parcels_train, epochs = 100, validation_split = 0.2)
+
+# Predict on sensors_test and round up the predictions
+preds = model.predict(sensors_test)
+preds_rounded = np.round(preds)
+
+# Print rounded preds
+print('Rounded Predictions: \n', preds_rounded)
+
+# Evaluate your model's accuracy on the test data
+accuracy = model.evaluate(sensors_test, parcels_test)[1]
+
+# Print accuracy
+print('Accuracy:', accuracy)
+
+#*****************************************************************************#
+
+#*******************************Keras callbacks*******************************#
+
+#The history callback#
+
+# Train your model and save its history
+h_callback = model.fit(X_train, y_train, epochs = 50,
+               validation_data=(X_test, y_test))
+
+# Plot train vs test loss during training
+plot_loss(h_callback.history['loss'], h_callback.history['val_loss'])
+
+# Plot train vs test accuracy during training
+plot_accuracy(h_callback.history['acc'], h_callback.history['val_acc'])
+
+#*****************************************************************************#
+
+#Early stopping your model#
+
+# Import the early stopping callback
+from keras.callbacks import EarlyStopping
+
+# Define a callback to monitor val_acc
+monitor_val_acc = EarlyStopping(monitor='val_acc',
+                       patience=5)
+
+# Train your model using the early stopping callback
+model.fit(X_train, y_train,
+           epochs=1000, validation_data=(X_test,y_test),
+           callbacks= [monitor_val_acc])
+
+#*****************************************************************************#
+
+#A combination of callbacks#
+
+# Import the EarlyStopping and ModelCheckpoint callbacks
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+# Early stop on validation accuracy
+monitor_val_acc = EarlyStopping(monitor = 'val_acc', patience = 3)
+
+# Save the best model as best_banknote_model.hdf5
+modelCheckpoint = ModelCheckpoint('best_banknote_model.hdf5', save_best_only = True)
+
+# Fit your model for a stupid amount of epochs
+h_callback = model.fit(X_train, y_train,
+                    epochs = 1000000000000,
+                    callbacks = [monitor_val_acc, modelCheckpoint],
+                    validation_data = (X_test, y_test))
+
+#*****************************************************************************#
