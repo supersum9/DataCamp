dbc473d7ba58fc3d0b9149920e2377a7ee399209
supersum9 <sumitbhardwaj@192-168-1-101.tpgi.com.au>
2021-01-09 13:23:44 +1100

    code

--- Image Processing in Python/Chapter 4 - Advanced Operations, Detecting Faces and Features.py
+++ Image Processing in Python/Chapter 4 - Advanced Operations, Detecting Faces and Features.py
@@ -74,3 +74,104 @@ show_image_with_corners(building_image, coords_w_min_40, \
 
 #********************************Face detection*******************************#
 
+#Is someone there?#
+
+# Load the trained file from data
+trained_file = data.lbp_frontal_face_cascade_filename()
+
+# Initialize the detector cascade
+detector = Cascade(trained_file)
+
+# Detect faces with min and max size of searching window
+detected = detector.detect_multi_scale(img = night_image,
+                                       scale_factor=1.2,
+                                       step_ratio=1,
+                                       min_size=(10, 10),
+                                       max_size=(200, 200))
+
+# Show the detected faces
+show_detected_face(night_image, detected)
+
+#*****************************************************************************#
+
+#Multiple faces#
+
+# Load the trained file from data
+trained_file = data.lbp_frontal_face_cascade_filename()
+
+# Initialize the detector cascade
+detector = Cascade(trained_file)
+
+# Detect faces with scale factor to 1.2 and step ratio to 1
+detected = detector.detect_multi_scale(img=friends_image,
+                                       scale_factor=1.2,
+                                       step_ratio=1,
+                                       min_size=(10, 10),
+                                       max_size=(200, 200))
+# Show the detected faces
+show_detected_face(friends_image, detected)
+
+#*****************************************************************************#
+
+#Segmentation and face detection#
+
+# Obtain the segmentation with default 100 regions
+segments = slic(profile_image, n_segments=100)
+
+# Obtain segmented image using label2rgb
+segmented_image = label2rgb(segments, profile_image, kind='avg')
+
+# Detect the faces with multi scale method
+detected = detector.detect_multi_scale(img=segmented_image,
+                                       scale_factor=1.2,
+                                       step_ratio=1,
+                                       min_size=(10, 10), max_size=(1000, 1000))
+
+# Show the detected faces
+show_detected_face(segmented_image, detected)
+
+#*****************************************************************************#
+
+#*************************Real-world applications*****************************#
+
+#Privacy protection#
+
+# Detect the faces
+detected = detector.detect_multi_scale(img=group_image,
+                                       scale_factor=1.2, step_ratio=1,
+                                       min_size=(10, 10), max_size=(100, 100))
+# For each detected face
+for d in detected:
+    # Obtain the face rectangle from detected coordinates
+    face = getFaceRectangle(d)
+
+    # Apply gaussian filter to extracted face
+    blurred_face = gaussian(face, multichannel= True, sigma = 8)
+
+    # Merge this blurry face to our final image and show it
+    resulting_image = mergeBlurryFace(group_image, blurred_face)
+show_image(resulting_image, "Blurred faces")
+
+#*****************************************************************************#
+
+#Help Sally restore her graduation photo#
+
+# Import the necessary modules
+from skimage.restoration import denoise_tv_chambolle, inpaint
+from skimage import transform
+
+# Transform the image so it's not rotated
+upright_img = rotate(damaged_image, 20)
+
+# Remove noise from the image, using the chambolle method
+upright_img_without_noise = denoise_tv_chambolle(upright_img,weight=0.1, \
+    multichannel=True)
+
+# Reconstruct the image missing parts
+mask = get_mask(upright_img)
+result = inpaint.inpaint_biharmonic(upright_img_without_noise, mask, \
+    multichannel=True)
+
+show_image(result)
+
+#*****************************************************************************#
