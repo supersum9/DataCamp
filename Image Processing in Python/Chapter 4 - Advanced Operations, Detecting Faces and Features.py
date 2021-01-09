9c1405890cdfe54d7600694a58c3bad19309ca8a
supersum9 <sumitbhardwaj@192-168-1-101.tpgi.com.au>
2021-01-09 13:07:24 +1100

    code

---
+++ Image Processing in Python/Chapter 4 - Advanced Operations, Detecting Faces and Features.py
@@ -0,0 +1,76 @@
+#****************************Finding the edges with Canny*********************#
+
+#Edges#
+
+# Import the canny edge detector
+from skimage.feature import canny
+
+# Convert image to grayscale
+grapefruit = color.rgb2gray(grapefruit)
+
+# Apply canny edge detector
+canny_edges = canny(grapefruit)
+
+# Show resulting image
+show_image(canny_edges, "Edges with Canny")
+
+#*****************************************************************************#
+
+#Less edgy#
+
+# Apply canny edge detector with a sigma of 1.8
+edges_1_8 = canny(grapefruit, sigma=1.8)
+
+# Apply canny edge detector with a sigma of 2.2
+edges_2_2 = canny(grapefruit, sigma=2.2)
+
+# Show resulting images
+show_image(edges_1_8, "Sigma of 1.8")
+show_image(edges_2_2, "Sigma of 2.2")
+
+#*****************************************************************************#
+
+#*************************Right around the corner*****************************#
+
+#Perspective#
+
+# Import the corner detector related functions and module
+from skimage.feature import corner_harris, corner_peaks
+
+# Convert image from RGB-3 to grayscale
+building_image_gray = color.rgb2gray(building_image)
+
+# Apply the detector  to measure the possible corners
+measure_image = corner_harris(building_image_gray)
+
+# Find the peaks of the corners using the Harris detector
+coords = corner_peaks(measure_image, min_distance=2)
+
+# Show original and resulting image with corners detected
+show_image(building_image, "Original")
+show_image_with_corners(building_image, coords)
+
+#*****************************************************************************#
+
+#Less corners#
+
+# Find the peaks with a min distance of 2 pixels
+coords_w_min_2 = corner_peaks(measure_image, min_distance=2)
+print("With a min_distance set to 2, we detect a total", len(coords_w_min_2), \
+    "corners in the image.")
+
+# Find the peaks with a min distance of 40 pixels
+coords_w_min_40 = corner_peaks(measure_image, min_distance=40)
+print("With a min_distance set to 40, we detect a total", \
+    len(coords_w_min_40), "corners in the image.")
+
+# Show original and resulting image with corners detected
+show_image_with_corners(building_image, coords_w_min_2, \
+    "Corners detected with 2 px of min_distance")
+show_image_with_corners(building_image, coords_w_min_40, \
+    "Corners detected with 40 px of min_distance")
+
+#*****************************************************************************#
+
+#********************************Face detection*******************************#
+
