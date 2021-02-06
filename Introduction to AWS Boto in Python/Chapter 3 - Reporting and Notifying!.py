cb127db3c445942a36c7b4fc4d274b536dfb5037
supersum9 <sumitbhardwaj@192-168-1-102.tpgi.com.au>
2021-02-06 16:59:25 +1100

    code

--- Introduction to AWS Boto in Python/Chapter 3 - Reporting and Notifying!.py
+++ Introduction to AWS Boto in Python/Chapter 3 - Reporting and Notifying!.py
@@ -54,3 +54,156 @@ print(sns.list_topics()['Topics'])
 #*****************************************************************************#
 
 #****************************SNS Subscriptions********************************#
+
+#Subscribing to topics#
+
+# Subscribe Elena's phone number to streets_critical topic
+resp_sms = sns.subscribe(
+  TopicArn = str_critical_arn,
+  Protocol='sms', Endpoint="+16196777733")
+
+# Print the SubscriptionArn
+print(resp_sms['SubscriptionArn'])
+
+# Subscribe Elena's email to streets_critical topic.
+resp_email = sns.subscribe(
+  TopicArn = str_critical_arn,
+  Protocol='email', Endpoint="eblock@sandiegocity.gov")
+
+# Print the SubscriptionArn
+print(resp_email['SubscriptionArn'])
+
+#*****************************************************************************#
+
+#Creating multiple subscriptions#
+
+# For each email in contacts, create subscription to street_critical
+for email in contacts['Email']:
+  sns.subscribe(TopicArn = str_critical_arn,
+                # Set channel and recipient
+                Protocol = 'email',
+                Endpoint = email)
+
+# List subscriptions for streets_critical topic, convert to DataFrame
+response = sns.list_subscriptions_by_topic(
+  TopicArn = str_critical_arn)
+subs = pd.DataFrame(response['Subscriptions'])
+
+# Preview the DataFrame
+subs.head()
+
+#*****************************************************************************#
+
+#Deleting multiple subscriptions
+
+# List subscriptions for streets_critical topic.
+response = sns.list_subscriptions_by_topic(
+  TopicArn = str_critical_arn)
+
+# For each subscription, if the protocol is SMS, unsubscribe
+for sub in response['Subscriptions']:
+  if sub['Protocol'] == 'sms':
+	  sns.unsubscribe(SubscriptionArn=sub['SubscriptionArn'])
+
+# List subscriptions for streets_critical topic in one line
+subs = sns.list_subscriptions_by_topic(
+  TopicArn=str_critical_arn)['Subscriptions']
+
+# Print the subscriptions
+print(subs)
+
+#*****************************************************************************#
+
+#****************************Sending messages*********************************#
+
+# If there are over 100 potholes, create a message
+if streets_v_count > 100:
+  # The message should contain the number of potholes.
+  message = "There are {} potholes!".format(streets_v_count)
+  # The email subject should also contain number of potholes
+  subject = "Latest pothole count is {}".format(streets_v_count)
+
+  # Publish the email to the streets_critical topic
+  sns.publish(
+    TopicArn = str_critical_arn,
+    # Set subject and message
+    Message = message,
+    Subject = subject
+  )
+
+#*****************************************************************************#
+
+#Sending a single SMS message#
+
+# Loop through every row in contacts
+for idx, row in contacts.iterrows():
+
+    # Publish an ad-hoc sms to the user's phone number
+    response = sns.publish(
+        # Set the phone number
+        PhoneNumber = str(row['Phone']),
+        # The message should include the user's name
+        Message = 'Hello {}'.format(row['Name'])
+    )
+
+    print(response)
+
+#*****************************************************************************#
+
+#****************Case Study: Building a notification system*******************#
+
+#Creating multi-level topics#
+
+dept_arns = {}
+
+for dept in departments:
+  # For each deparment, create a critical topic
+  critical = sns.create_topic(Name="{}_critical".format(dept))
+  # For each department, create an extreme topic
+  extreme = sns.create_topic(Name="{}_extreme".format(dept))
+  # Place the created TopicARNs into a dictionary
+  dept_arns['{}_critical'.format(dept)] = critical['TopicArn']
+  dept_arns['{}_extreme'.format(dept)] = extreme['TopicArn']
+
+# Print the filled dictionary.
+print(dept_arns)
+
+#*****************************************************************************#
+
+#Different protocols per topic level#
+
+for index, user_row in contacts.iterrows():
+  # Get topic names for the users's dept
+  critical_tname = '{}_critical'.format(user_row['Department'])
+  extreme_tname = '{}_extreme'.format(user_row['Department'])
+
+  # Get or create the TopicArns for a user's department.
+  critical_arn = sns.create_topic(Name=critical_tname)['TopicArn']
+  extreme_arn = sns.create_topic(Name=extreme_tname)['TopicArn']
+
+  # Subscribe each users email to the critical Topic
+  sns.subscribe(TopicArn = critical_arn,
+                Protocol='email', Endpoint=user_row['Email'])
+  # Subscribe each users phone number for the extreme Topic
+  sns.subscribe(TopicArn = extreme_arn,
+                Protocol='sms', Endpoint=str(user_row['Phone']))
+
+#*****************************************************************************#
+
+#Sending multi-level alerts#
+
+if vcounts['water'] > 100:
+  # If over 100 water violations, publish to water_critical
+  sns.publish(
+    TopicArn = dept_arns['water_critical'],
+    Message = "{} water issues".format(vcounts['water']),
+    Subject = "Help fix water violations NOW!")
+
+if vcounts['water'] > 300:
+  # If over 300 violations, publish to water_extreme
+  sns.publish(
+    TopicArn = dept_arns['water_extreme'],
+    Message = "{} violations! RUN!".format(vcounts['water']),
+    Subject = "THIS IS BAD.  WE ARE FLOODING!")
+
+#*****************************************************************************#
